bbfacc7b71b691bfc229cb0a06021bdc20d80ab1
sanbian <sanbian@shulie.io>
2017-08-07 16:54:54 +0800

    update

---
+++ src/main/java/com/self/ddyoung/rice/model/WriteoffDO.java
@@ -0,0 +1,15 @@
+package com.self.ddyoung.rice.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * Created by sanbian on 2017/8/3.
+ */
+@Entity
+public class WriteoffDO implements Serializable{
+
+    private static final long serialVersionUID = -289600146454844986L;
+
+
+}
